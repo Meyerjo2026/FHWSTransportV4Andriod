d302850612de9c71848baadfdcb3d7095651ae2d
John Meyer <johnmeyer@Johns-MacBook-Pro.local>
2026-09-21 05:12:01 +0200

    android: enable WebView DevTools in debug builds

--- android/app/src/main/java/com/fhws/transport/MainActivity.kt
+++ android/app/src/main/java/com/fhws/transport/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.fhws.transport
 
 import android.os.Bundle
+import android.webkit.WebView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -10,6 +11,9 @@ import com.fhws.transport.ui.theme.FhwsTransportTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        if (BuildConfig.DEBUG) {
+            WebView.setWebContentsDebuggingEnabled(true)
+        }
         enableEdgeToEdge()
         setContent {
             FhwsTransportTheme {
